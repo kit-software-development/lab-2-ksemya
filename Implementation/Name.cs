6a9f55e99162b9062b634ddb521535deeb43fc59
ksemya <47814139+ksemya@users.noreply.github.com>
2019-04-21 14:04:43 +0300

    Add files via upload

---
+++ Implementation/Name.cs
@@ -0,0 +1,50 @@
+﻿namespace Practice.Common
+{
+    /// <summary>
+    ///     Скрытая реализация представления об имени человека.
+    /// </summary>
+    internal struct Name:IName
+    {
+        /*
+         * TODO #1: Реализуйте интерфейс IName для структуры Name
+         */
+
+        /// <summary>
+        ///     Имя.
+        /// </summary>
+        public string FirstName { get; set; }
+        
+        /// <summary>
+        ///     Фамилия.
+        /// </summary>
+        public string Surname { get; set;  }
+        
+        /// <summary>
+        ///     Отчество.
+        /// </summary>
+        public string Patronymic { get; set; }
+
+        public string FullName { get; set;  }
+
+        public string ShortName { get; set;  }
+
+        public Name(string surname, string firstname, string patronymic)
+        {
+            FirstName = firstname;
+            Surname = surname;
+            Patronymic = patronymic;
+            FullName = surname + ' ' + firstname + ' ' + patronymic;
+            ShortName = surname + ' ' + firstname[0] + ". " + patronymic[0] + '.';
+        }
+
+        public Name(IName name)
+        {
+            FullName = name.FullName;
+            ShortName = name.ShortName;
+            var arr = FullName.Split(' ');
+            Surname = arr[0];
+            FirstName = arr[1];
+            Patronymic = arr[2];
+        }
+    }
+}
